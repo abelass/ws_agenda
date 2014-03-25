aa4d13c7dbbbfae39f2cc5b7e171c0f63890eede
Rainer <rainer@websimple.be>
2014-03-25 17:44:16 +0100

    import initial

---
+++ lang/paquet-ws_agenda_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// W
+	'ws_agenda_description' => '',
+	'ws_agenda_nom' => 'Web Simple Agenda',
+	'ws_agenda_slogan' => '',
+);
+
+?>
